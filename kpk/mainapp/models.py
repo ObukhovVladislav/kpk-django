835abe37dc656ab5ed172c269a6e07ffd561d784
Vladislav Obv <VladislavObv@example.com>
2020-10-19 10:39:08 +0500

    first models

--- kpk/mainapp/models.py
+++ kpk/mainapp/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class SubjectCategory(models.Model):
     name = models.CharField(max_length=128)
     desc = models.TextField(blank=True)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    hours = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
