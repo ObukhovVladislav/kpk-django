493762abfafcef040a2e40c3e1cb930e1caa1e8e
Vladislav Obv <VladislavObv@example.com>
2020-10-19 11:06:05 +0500

    first models

--- kpk/mainapp/admin.py
+++ kpk/mainapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from mainapp.models import SubjectCategory
+from mainapp.models import SubjectCategory, Course
+
+
 
 admin.site.register(SubjectCategory)
+admin.site.register(Course)
--- kpk/mainapp/models.py
+++ kpk/mainapp/models.py
@@ -5,9 +5,15 @@ class SubjectCategory(models.Model):
     name = models.CharField(max_length=128)
     desc = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Course(models.Model):
+    category = models.ForeignKey(SubjectCategory,
+                                 on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True)
     hours = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
+
